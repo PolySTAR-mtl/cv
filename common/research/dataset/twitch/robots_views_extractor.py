2f6994e861d04ca892bbc93fb762751a99ba7c05
Mathieu Beligon <mathieu@feedly.com>
2019-12-10 00:02:41 -0500

    [common] (dset) use tqdm for the progress bar

--- common/research/dataset/twitch/robots_views_extractor.py
+++ common/research/dataset/twitch/robots_views_extractor.py
@@ -5,6 +5,7 @@ import cv2
 import ffmpeg
 from pyprind import ProgBar
 import numpy as np
+from tqdm import tqdm
 
 from research.constants import TWITCH_DSET
 from research.dataset.twitch.robot_view import is_image_from_robot_view
@@ -24,26 +25,18 @@ class RobotsViewExtractor:
         self.frame_generator: VideoFrameGenerator = VideoFrameGenerator(self.video_path, self.FPS)
 
     def run(self):
-        self._create_prog_bar()
-        self._start_extraction()
-
-    def _start_extraction(self):
-        for i, frame in enumerate(self.frame_generator.generate()):
+        for i, frame in tqdm(
+            enumerate(self.frame_generator.generate()),
+            total=self._get_number_of_frames(),
+            desc=f'Extracting robots views from video {self.video_name}.mp4',
+            unit='frames',
+            ncols=200,
+        ):
             self._process_frame(frame, i)
-
-    def _create_prog_bar(self):
-        self._prog_bar = ProgBar(
-            self._get_number_of_frames(),
-            title='Creating thumbnails',
-            width=100,
-            stream=sys.stdout,
-            update_interval=True,
-        )
 
     def _process_frame(self, frame: np.ndarray, frame_number: int):
         if is_image_from_robot_view(frame):
             self._save_frame(frame, frame_number)
-        self._prog_bar.update()
 
     def _save_frame(self, frame: np.ndarray, frame_number: int):
         cv2.imwrite(f"{RES_DIR}/{self.video_name}-frame-{frame_number + 1:06}.jpg", frame)
