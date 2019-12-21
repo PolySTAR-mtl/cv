b6a4b090fbf8e7fd512cb9ef5311564a91dbea0a
Mathieu Beligon <mathieu@feedly.com>
2019-12-21 12:01:12 -0500

    [common] (twitch-dset) Add aerial view detection

---
+++ common/research_common/dataset/twitch/aerial_view_detector.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from skimage import io
+
+from research_common.constants import TWITCH_DSET
+from research_common.dataset.twitch.mask_detector import MaskDetector
+
+aerial_view_detector = MaskDetector(
+    Path(__file__).parent / 'mask_aerial.jpg',
+    [
+        (527, 528, 292, 297, 20),
+        (527, 531, 303, 303, 20),
+        (532, 537, 286, 287, 20),
+        (536, 541, 302, 303, 20),
+        (543, 544, 292, 297, 20),
+        (535, 535, 292, 297, 20),
+    ]
+)
+
+if __name__ == '__main__':
+    for file_path in sorted((TWITCH_DSET / 'robots-views').glob('*.jpg')):
+        if aerial_view_detector.is_matching(io.imread(str(file_path))):
+            print(file_path.name)
