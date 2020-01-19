97b1b4935aeaa1deb61b58d4b4774001c6649c1f
Mathieu Beligon <mathieu@feedly.com>
2020-01-19 11:00:31 -0500

    [common] (constants) typo add DIR in constant names

--- common/research_common/constants.py
+++ common/research_common/constants.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 
+DSET_DIR = Path(__file__).parent.parent.parent / "dataset"
 
-DSET_DIR = Path(__file__).parent.parent.parent / 'dataset'
+TWITCH_DSET_DIR: Path = DSET_DIR / "twitch"
+ROCO_DSET_DIR: Path = DSET_DIR / "dji_roco"
+TENSORFLOW_RECORDS_DIR: Path = DSET_DIR / "tf_records"
 
-TWITCH_DSET: Path = DSET_DIR / 'twitch'
-ROCO_DSET: Path = DSET_DIR / 'dji_roco'
+TWITCH_DSET_DIR.mkdir(parents=True, exist_ok=True)
+ROCO_DSET_DIR.mkdir(parents=True, exist_ok=True)
+TENSORFLOW_RECORDS_DIR.mkdir(parents=True, exist_ok=True)
