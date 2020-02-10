e6b9361200c9579abc13e2386d4b47995400e437
Mathieu Beligon <mathieu@feedly.com>
2020-02-09 23:21:53 -0500

    [common] (view) Add easy way to display image annotation

--- common/polystar/common/view/display_object_on_image.py
+++ common/polystar/common/view/display_object_on_image.py
@@ -1,15 +1,7 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
+from polystar.common.models.image import Image
 from polystar.common.models.object import Object
+from polystar.common.view.display_image_annotation import display_image_with_objects
 
 
-def display_object(image: np.ndarray, obj: Object):
-    plt.figure(figsize=(16, 12))
-    plt.imshow(image)
-    color = "red"
-    rect = plt.Rectangle((obj.x, obj.y), obj.w, obj.h, linewidth=1, edgecolor=color, fill=False)
-    print((obj.x, obj.y), obj.w, obj.h)
-    plt.gca().add_patch(rect)
-    plt.text(obj.x, obj.y - 2, f"{obj.type.name} ({int(obj.confidence * 100)}%)", color=color)
-    plt.show()
+def display_object(image: Image, obj: Object):
+    display_image_with_objects(image, [obj])
