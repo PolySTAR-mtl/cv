dbb8eb3baedb7314c5bd920e08cabac64ae28d79
Mathieu Beligon <mathieu@feedly.com>
2020-03-03 12:00:49 -0500

    [common] (tf) back to tf 2.1 for demos

--- robots-at-robots/polystar/robots_at_robots/scripts/demo_infer.py
+++ robots-at-robots/polystar/robots_at_robots/scripts/demo_infer.py
@@ -1,5 +1,6 @@
 from polystar.common.pipeline.objects_detectors.tf_model_objects_detector import TFModelObjectsDetector
 from polystar.common.pipeline.objects_validators.confidence_object_validator import ConfidenceObjectValidator
+from polystar.common.utils.tensorflow import patch_tf_v2
 from polystar.common.view.display_image_annotation import display_image_with_objects
 from polystar.robots_at_robots.dependency_injection import make_injector
 from research_common.dataset.roco.roco_datasets import ROCODataset
@@ -7,6 +8,7 @@ from research_common.dataset.split import Split
 from research_common.dataset.split_dataset import SplitDataset
 
 if __name__ == "__main__":
+    patch_tf_v2()
     injector = make_injector()
 
     objects_detector = injector.get(TFModelObjectsDetector)
