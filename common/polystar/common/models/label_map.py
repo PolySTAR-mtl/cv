a17bf9d58b97d37226153d22ee4453f9ed0ec4b5
Mathieu Beligon <mathieu@feedly.com>
2020-03-03 13:35:28 -0500

    [common] (labelmap) remove usage of tf models code

--- common/polystar/common/models/label_map.py
+++ common/polystar/common/models/label_map.py
@@ -1,8 +1,7 @@
+import re
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict, Any
-
-from object_detection.utils import label_map_util
+from typing import Dict, Any, List
 
 
 @dataclass
@@ -18,10 +17,15 @@ class LabelMap:
 
     @staticmethod
     def from_file(file_path: Path) -> "LabelMap":
-        return LabelMap.from_dict(label_map_util.create_category_index_from_labelmap(str(file_path)))
+        matches: List[re.Match] = re.findall(
+            r"item *?{" r".*?" r"id: (?P<id>\d)" r".*?" r"name: '(?P<name>\w*)'" r".*?" r"\}",
+            file_path.read_text(),
+            re.S,
+        )
+        return LabelMap.from_dict({int(i): n for i, n in matches})
 
     @staticmethod
-    def from_dict(d: Dict[str, Dict[str, Any]]) -> "LabelMap":
-        name2id = {name_id["name"]: name_id["id"] for name_id in d.values()}
-        id2name = {name_id["id"]: name_id["name"] for name_id in d.values()}
+    def from_dict(d: Dict[int, Any]) -> "LabelMap":
+        name2id = {n: int(i) for i, n in d.items()}
+        id2name = d
         return LabelMap(id2name=id2name, name2id=name2id)
