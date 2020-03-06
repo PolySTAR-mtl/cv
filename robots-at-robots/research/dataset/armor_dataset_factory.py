cdd89b49f54043e46dbf74556568a3c3cedc5bd9
Mathieu Beligon <mathieu@feedly.com>
2020-03-06 17:47:53 -0500

    [robots-at-robots] (armor dset factory) return image path and armor id to know where the armor came from

--- robots-at-robots/research/dataset/armor_dataset_factory.py
+++ robots-at-robots/research/dataset/armor_dataset_factory.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import List, Iterable, Tuple
 
 import matplotlib.pyplot as plt
@@ -12,25 +13,28 @@ from research_common.dataset.roco_dataset import ROCODataset
 
 class ArmorDatasetFactory:
     @staticmethod
-    def from_image_annotation(image_annotation: ImageAnnotation) -> Iterable[Tuple[Image, ArmorColor, int]]:
+    def from_image_annotation(
+        image_annotation: ImageAnnotation,
+    ) -> Iterable[Tuple[Image, ArmorColor, ArmorNumber, int, Path]]:
         img = image_annotation.image
         armors: List[Armor] = TypeObjectValidator(ObjectType.Armor).filter(image_annotation.objects, img)
-        for obj in armors:
-            yield img[obj.y : obj.y + obj.h, obj.x : obj.x + obj.w], obj.color, obj.numero
+        for i, obj in enumerate(armors):
+            croped_img = img[obj.y : obj.y + obj.h, obj.x : obj.x + obj.w]
+            yield croped_img, obj.color, obj.numero, i, image_annotation.image_path
 
     @staticmethod
-    def from_dataset(dataset: ROCODataset) -> Iterable[Tuple[Image, ArmorColor, ArmorNumber]]:
+    def from_dataset(dataset: ROCODataset) -> Iterable[Tuple[Image, ArmorColor, ArmorNumber, int, Path]]:
         for image_annotation in dataset.image_annotations:
             for rv in ArmorDatasetFactory.from_image_annotation(image_annotation):
                 yield rv
 
 
 if __name__ == "__main__":
-    for i, (armor_img, c, n) in enumerate(ArmorDatasetFactory.from_dataset(DJIROCODataset.CentralChina)):
-        print(c, n)
+    for i, (armor_img, c, n, k, p) in enumerate(ArmorDatasetFactory.from_dataset(DJIROCODataset.CentralChina)):
+        print(c, n, k, "in", p)
         plt.imshow(armor_img)
         plt.show()
         plt.clf()
 
-        if i == 15:
+        if i == 50:
             break
