44a6a54663a10a1007a70726ea61c01a3e9bb487
Mathieu Beligon <mathieu@feedly.com>
2020-03-07 14:54:09 -0500

    [common] (constants) Add twitch final dset path to constants

--- common/research_common/constants.py
+++ common/research_common/constants.py
@@ -6,8 +6,10 @@ TWITCH_DSET_DIR: Path = DSET_DIR / "twitch"
 ROCO_DSET_DIR: Path = DSET_DIR / "dji_roco"
 TENSORFLOW_RECORDS_DIR: Path = DSET_DIR / "tf_records"
 TWITCH_ROBOTS_VIEWS_DIR: Path = TWITCH_DSET_DIR / "robots-views"
+TWITCH_DSET_ROBOTS_VIEWS_DIR: Path = TWITCH_DSET_DIR / "final-robots-views"
 
 TWITCH_DSET_DIR.mkdir(parents=True, exist_ok=True)
 ROCO_DSET_DIR.mkdir(parents=True, exist_ok=True)
 TENSORFLOW_RECORDS_DIR.mkdir(parents=True, exist_ok=True)
 TWITCH_ROBOTS_VIEWS_DIR.mkdir(parents=True, exist_ok=True)
+TWITCH_DSET_ROBOTS_VIEWS_DIR.mkdir(parents=True, exist_ok=True)
