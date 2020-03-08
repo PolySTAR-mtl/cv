7d091321c7de9f6c54eac803c05fa05359067c45
Mathieu Beligon <mathieu@feedly.com>
2020-03-08 17:18:56 -0400

    [robots] (demo) fix camera opening

--- robots-at-robots/research/demos/demo_pipeline_camera.py
+++ robots-at-robots/research/demos/demo_pipeline_camera.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import cv2
 
 from polystar.common.models.camera import Camera
@@ -14,6 +16,39 @@ from research_common.dataset.dji.dji_roco_datasets import DJIROCODataset
 from research_common.dataset.split import Split
 from research_common.dataset.split_dataset import SplitDataset
 
+
+def open_cam_onboard(width, height):
+    """Open the Jetson onboard camera."""
+    gst_elements = str(subprocess.check_output("gst-inspect-1.0"))
+    if "nvcamerasrc" in gst_elements:
+        # On versions of L4T prior to 28.1, you might need to add
+        # 'flip-method=2' into gst_str below.
+        gst_str = (
+            "nvcamerasrc ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)2592, height=(int)1458, "
+            "format=(string)I420, framerate=(fraction)30/1 ! "
+            "nvvidconv ! "
+            "video/x-raw, width=(int){}, height=(int){}, "
+            "format=(string)BGRx ! "
+            "videoconvert ! appsink"
+        ).format(width, height)
+    elif "nvarguscamerasrc" in gst_elements:
+        gst_str = (
+            "nvarguscamerasrc ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)1920, height=(int)1080, "
+            "format=(string)NV12, framerate=(fraction)30/1 ! "
+            "nvvidconv flip-method=2 ! "
+            "video/x-raw, width=(int){}, height=(int){}, "
+            "format=(string)BGRx ! "
+            "videoconvert ! appsink"
+        ).format(width, height)
+    else:
+        raise RuntimeError("onboard camera source not found!")
+    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+
 if __name__ == "__main__":
     patch_tf_v2()
     injector = make_injector()
@@ -21,7 +56,7 @@ if __name__ == "__main__":
     # objects_detector = injector.get(TFModelObjectsDetector)
     # filters = [ConfidenceObjectValidator(confidence_threshold=0.5)]
 
-    cap = cv2.VideoCapture(0)
+    cap = open_cam_onboard(0)
 
     for i, image_path in enumerate(SplitDataset(DJIROCODataset.CentralChina, Split.Test).image_paths):
         ret, image = cap.read()
