9902cb043aca72bb735d2b0f05a4db809278c891
Mathieu Beligon <mathieu@feedly.com>
2020-03-08 16:56:58 -0400

    [common] (clean) remove tf models from dependencies

--- common/polystar/common/utils/tensorflow.py
+++ common/polystar/common/utils/tensorflow.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 
-from object_detection.utils import ops as utils_ops
+# from object_detection.utils import ops as utils_ops
 
 
 def patch_tf_v2():
-    # patch tf1 into `utils.ops`
-    utils_ops.tf = tf.compat.v1
+    # # patch tf1 into `utils.ops`
+    # utils_ops.tf = tf.compat.v1
 
     # Patch the location of gfile
     tf.gfile = tf.io.gfile
