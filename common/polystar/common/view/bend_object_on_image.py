7919ad0baf532c980297db32073b67ea2a8def19
Mathieu Beligon <mathieu.beligon@gmail.com>
2020-03-09 18:59:59 -0400

    [common] (viz) fix cv2 object visualisation

--- common/polystar/common/view/bend_object_on_image.py
+++ common/polystar/common/view/bend_object_on_image.py
@@ -1,21 +1,23 @@
 import cv2
+from typing import Tuple
+
 import numpy as np
 
 from polystar.common.models.image import Image
 from polystar.common.models.object import Object
 
 _COLORS = [
-    (0.12156862745098039, 0.4666666666666667, 0.7058823529411765),
-    (1.0, 0.4980392156862745, 0.054901960784313725),
-    (0.17254901960784313, 0.6274509803921569, 0.17254901960784313),
-    (0.8392156862745098, 0.15294117647058825, 0.1568627450980392),
-    (0.5803921568627451, 0.403921568627451, 0.7411764705882353),
-    (0.5490196078431373, 0.33725490196078434, 0.29411764705882354),
-    (0.8901960784313725, 0.4666666666666667, 0.7607843137254902),
-    (0.4980392156862745, 0.4980392156862745, 0.4980392156862745),
-    (0.7372549019607844, 0.7411764705882353, 0.13333333333333333),
-    (0.09019607843137255, 0.7450980392156863, 0.8117647058823529),
-]  # seaborn.color_palette()
+    [31, 119, 180],
+    [255, 127, 14],
+    [44, 160, 44],
+    [214, 39, 40],
+    [148, 103, 189],
+    [140, 86, 75],
+    [227, 119, 194],
+    [127, 127, 127],
+    [188, 189, 34],
+    [23, 190, 207],
+]  # seaborn.color_palette() * 255
 
 ALPHA = 0.5
 FONT = cv2.FONT_HERSHEY_PLAIN
@@ -25,30 +27,32 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 
-def bend_object_on_image(image: Image, obj: Object):
-    assert image.dtype == np.uint8
-    img_h, img_w, _ = image.shape
+def bend_boxed_text_on_image(img: Image, text: str, topleft: Tuple[int, int], color: Tuple[int, int, int]):
+    assert img.dtype == np.uint8
+    img_h, img_w, _ = img.shape
+    if topleft[0] >= img_w or topleft[1] >= img_h:
+        return img
     margin = 3
-    size = cv2.getTextSize(obj.type.name, FONT, TEXT_SCALE, TEXT_THICKNESS)
+    size = cv2.getTextSize(text, FONT, TEXT_SCALE, TEXT_THICKNESS)
     w = size[0][0] + margin * 2
     h = size[0][1] + margin * 2
     # the patch is used to draw boxed text
     patch = np.zeros((h, w, 3), dtype=np.uint8)
-    patch[...] = _COLORS[obj.type.value]
-    cv2.putText(
-        patch,
-        obj.type.name,
-        (margin + 1, h - margin - 2),
-        FONT,
-        TEXT_SCALE,
-        WHITE,
-        thickness=TEXT_THICKNESS,
-        lineType=cv2.LINE_8,
-    )
-    cv2.rectangle(patch, (0, 0), (w - 1, h - 1), BLACK, thickness=1)
-    w = min(w, img_w - obj.x)  # clip overlay at image boundary
-    h = min(h, img_h - obj.y)
+    patch[...] = color
+    cv2.putText(patch, text, (margin+1, h-margin-2), FONT, TEXT_SCALE,
+                WHITE, thickness=TEXT_THICKNESS, lineType=cv2.LINE_8)
+    cv2.rectangle(patch, (0, 0), (w-1, h-1), BLACK, thickness=1)
+    w = min(w, img_w - topleft[0])  # clip overlay at image boundary
+    h = min(h, img_h - topleft[1])
     # Overlay the boxed text onto region of interest (roi) in img
-    roi = image[obj.y : obj.y + h, obj.x : obj.x + w, :]
+    roi = img[topleft[1]:topleft[1]+h, topleft[0]:topleft[0]+w, :]
     cv2.addWeighted(patch[0:h, 0:w, :], ALPHA, roi, 1 - ALPHA, 0, roi)
+    return img
+
+
+def bend_object_on_image(image: Image, obj: Object):
+    color = _COLORS[obj.type.value]
+    cv2.rectangle(image, (obj.x, obj.y), (obj.x + obj.w, obj.y + obj.h), color, 2)
+
+    bend_boxed_text_on_image(image, f"{obj.type.name} ({obj.confidence:.1%})", (obj.x, obj.y), _COLORS[obj.type.value])
     return image
