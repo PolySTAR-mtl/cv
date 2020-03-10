5990760f1cf4e6c35e7ea36b0551f98ff17c1c2f
Mathieu Beligon <mathieu@feedly.com>
2020-03-10 00:12:28 -0400

    [robots] (demo) fix: stop using settings to get model name demo (temporary)

--- robots-at-robots/research/demos/utils.py
+++ robots-at-robots/research/demos/utils.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
 
 from polystar.common.constants import MODELS_DIR
-from polystar.robots_at_robots.globals import settings
 
 
 def load_tf_model():
-    model = tf.saved_model.load(export_dir=str(MODELS_DIR / settings.MODEL_NAME / "saved_model"))
+    model = tf.saved_model.load(export_dir=str(MODELS_DIR / "robots/ssd_mobilenet_v2_coco_2018_03_29" / "saved_model"))
     return model.signatures["serving_default"]
