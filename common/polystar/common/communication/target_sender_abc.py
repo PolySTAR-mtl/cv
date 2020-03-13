2d27f312709dbc2724fb649e761d103cc5a19dd5
Mathieu Beligon <mathieu@feedly.com>
2020-03-13 18:35:08 -0400

    [common] (TargetSenderABC) add the TargetSenderABC

---
+++ common/polystar/common/communication/target_sender_abc.py
@@ -0,0 +1,13 @@
+import json
+from abc import ABC, abstractmethod
+
+from polystar.common.models.target_abc import TargetABC
+
+
+class TargetSenderABC(ABC):
+    def send(self, target: TargetABC):
+        self._send_text(json.dumps(target.to_json()))
+
+    @abstractmethod
+    def _send_text(self, text: str):
+        pass
