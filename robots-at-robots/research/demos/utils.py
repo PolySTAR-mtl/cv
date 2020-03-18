2247f8aa84b216596b1b5741a95ab0b854e65744
Mathieu Beligon <mathieu@feedly.com>
2020-03-18 12:28:53 -0400

    [robots-at-robots] (demo) Change model in use

--- robots-at-robots/research/demos/utils.py
+++ robots-at-robots/research/demos/utils.py
@@ -4,5 +4,12 @@ from polystar.common.constants import MODELS_DIR
 
 
 def load_tf_model():
-    model = tf.saved_model.load(export_dir=str(MODELS_DIR / "robots/ssd_mobilenet_v2_coco_2018_03_29" / "saved_model"))
+    model = tf.saved_model.load(
+        export_dir=str(
+            MODELS_DIR
+            / "robots"
+            / "ssd_mobilenet_v2_roco_2018_03_29_20200314_015411_TWITCH_TEMP_733_IMGS_29595steps"
+            / "saved_model"
+        )
+    )
     return model.signatures["serving_default"]
