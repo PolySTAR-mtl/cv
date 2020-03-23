5f1331922f6ef7b9d6a422ed208f460d297ba27a
Mathieu Beligon <mathieu@feedly.com>
2020-03-23 17:52:01 -0400

    [common] (models) fix typing

--- common/polystar/common/image_pipeline/models/random_model.py
+++ common/polystar/common/image_pipeline/models/random_model.py
@@ -12,14 +12,14 @@ class RandomModel(ModelABC):
     labels_: np.ndarray = field(init=False, default=None)
     weights_: np.ndarray = field(init=False, default=None)
 
-    def fit(self, features: Any, labels: List[Any]) -> "RandomModel":
+    def fit(self, features: List[Any], labels: List[Any]) -> "RandomModel":
         counter = Counter(labels)
         self.labels_ = np.asarray(list(counter.keys()))
         occurrences = np.asarray(list(counter.values()))
         self.weights_ = occurrences / occurrences.sum()
         return self
 
-    def predict(self, features: Any) -> Sequence[Any]:
+    def predict(self, features: List[Any]) -> Sequence[Any]:
         return np.random.choice(self.labels_, size=len(features), replace=True, p=self.weights_)
 
     def __str__(self) -> str:
