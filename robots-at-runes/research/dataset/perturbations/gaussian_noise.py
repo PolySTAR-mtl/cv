6053cfcfaee7c9b297826364147e2fb844d401ad
Yasmine Moumou <yasmine.moumou@polymtl.ca>
2020-06-03 09:28:48 +0000

    [robots-at-runes](perturbations) ajout de la fonction de randomisation des intensités et de sauvgarde des images

---
+++ robots-at-runes/research/dataset/perturbations/gaussian_noise.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from polystar.common.models.image import Image
+
+
+def gaussian_noise(image: Image, intensity: float) -> Image:
+    MAX_VARIANCE = 300
+    mean = 0
+    variance = MAX_VARIANCE * intensity
+    sigma = variance ** 0.5
+    row, column, ch = image.shape
+    gaussian = np.random.normal(mean, sigma, (row, column, ch))
+    perturbed_image = np.clip((image.astype(np.uint16) + gaussian), 0, 255).astype(np.uint8)
+    return perturbed_image
+
+
+if __name__ == "__main__":
+    from research.dataset.perturbations.utils import simple_perturbator_test
+
+    simple_perturbator_test(gaussian_noise)
---
+++ robots-at-runes/research/dataset/perturbations/saturation.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+from polystar.common.models.image import Image
+
+
+def saturation(image: Image, intensity: float) -> Image:
+    MAX_SATURATION = 0.6
+    saturation_factor = 1 + MAX_SATURATION * intensity
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    (h, s, v) = cv2.split(image_hsv)
+    new_s = np.clip((s.astype(np.uint16) * saturation_factor), 0, 255).astype(np.uint8)
+    new_image_hsv = cv2.merge([h, new_s, v])
+    image_rgb = cv2.cvtColor(new_image_hsv, cv2.COLOR_HSV2RGB).astype(np.uint8)
+    return image_rgb
+
+
+if __name__ == "__main__":
+    from research.dataset.perturbations.utils import simple_perturbator_test
+
+    simple_perturbator_test(saturation)
