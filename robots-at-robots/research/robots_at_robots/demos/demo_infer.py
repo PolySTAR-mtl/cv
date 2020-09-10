05a029301dae457b4ca80b2a3e294270305e7998
Mathieu Beligon <mathieu@feedly.com>
2020-09-10 23:02:54 +0200

    [common] (dataset) remove the split datasets

--- robots-at-robots/research/robots_at_robots/demos/demo_infer.py
+++ robots-at-robots/research/robots_at_robots/demos/demo_infer.py
@@ -1,12 +1,12 @@
 from polystar.common.models.label_map import LabelMap
-from polystar.common.target_pipeline.objects_detectors.tf_model_objects_detector import TFModelObjectsDetector
-from polystar.common.target_pipeline.objects_validators.confidence_object_validator import ConfidenceObjectValidator
+from polystar.common.target_pipeline.objects_detectors.tf_model_objects_detector import \
+    TFModelObjectsDetector
+from polystar.common.target_pipeline.objects_validators.confidence_object_validator import \
+    ConfidenceObjectValidator
 from polystar.common.utils.tensorflow import patch_tf_v2
 from polystar.common.view.plt_results_viewer import PltResultViewer
 from polystar.robots_at_robots.dependency_injection import make_injector
 from research.common.dataset.dji.dji_roco_datasets import DJIROCODataset
-from research.common.dataset.split import Split
-from research.common.dataset.split_dataset import SplitDataset
 from research.robots_at_robots.demos.utils import load_tf_model
 
 if __name__ == "__main__":
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     filters = [ConfidenceObjectValidator(confidence_threshold=0.5)]
 
     with PltResultViewer("Demo of tf model") as viewer:
-        for i, image in enumerate(SplitDataset(DJIROCODataset.CentralChina, Split.Test).images):
+        for i, image in enumerate(DJIROCODataset.CentralChina.images):
             objects = objects_detector.detect(image)
             for f in filters:
                 objects = f.filter(objects, image)
