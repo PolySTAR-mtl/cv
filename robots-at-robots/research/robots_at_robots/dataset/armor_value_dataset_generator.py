04e607b07bbc22ded1394630fc7a713067027385
Mathieu Beligon <mathieu@feedly.com>
2020-12-12 12:15:55 +0100

    [robots@robots] (armor) add way to exclude some files

--- robots-at-robots/research/robots_at_robots/dataset/armor_value_dataset_generator.py
+++ robots-at-robots/research/robots_at_robots/dataset/armor_value_dataset_generator.py
@@ -1,12 +1,20 @@
+from pathlib import Path
 from typing import Generic, List
 
+from polystar.common.filters.exclude_filter import ExcludeFilter
 from polystar.common.filters.filter_abc import FilterABC
 from polystar.common.filters.pass_through_filter import PassThroughFilter
 from research.common.datasets.image_file_dataset_builder import DirectoryDatasetBuilder
 from research.common.datasets.lazy_dataset import TargetT
 from research.common.datasets.roco.roco_dataset_builder import ROCODatasetBuilder
+from research.robots_at_robots.dataset.armor_dataset_changes import ArmorDatasetChanges
 from research.robots_at_robots.dataset.armor_value_dataset_cache import ArmorValueDatasetCache
 from research.robots_at_robots.dataset.armor_value_target_factory import ArmorValueTargetFactory
+
+
+class ExcludeFilesFilter(ExcludeFilter[Path]):
+    def validate_single(self, path: Path) -> bool:
+        return path.name not in self.to_remove
 
 
 class ArmorValueDatasetGenerator(Generic[TargetT]):
@@ -27,9 +35,10 @@ class ArmorValueDatasetGenerator(Generic[TargetT]):
         cache_dir = roco_dataset_builder.main_dir / self.task_name
         dataset_name = f"{roco_dataset_builder.name}_armor_{self.task_name}"
 
-        cache = ArmorValueDatasetCache(roco_dataset_builder, cache_dir, dataset_name, self.target_factory)
-        cache.generate_if_needed()
+        ArmorValueDatasetCache(roco_dataset_builder, cache_dir, dataset_name, self.target_factory).generate_if_needed()
 
-        return DirectoryDatasetBuilder(cache_dir, self.target_factory.from_file, dataset_name).filter_targets(
-            self.targets_filter
+        return (
+            DirectoryDatasetBuilder(cache_dir, self.target_factory.from_file, dataset_name)
+            .filter_targets(self.targets_filter)
+            .filter_examples(ExcludeFilesFilter(ArmorDatasetChanges(cache_dir).invalidated))
         )
