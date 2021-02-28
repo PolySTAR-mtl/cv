60eb67168dfe564233132d9aa1ae56165a103817
Mathieu Beligon <mathieu@feedly.com>
2021-02-27 20:57:50 -0500

    [CSLink] Add methods to easily send target

--- src/polystar/communication/cs_link_abc.py
+++ src/polystar/communication/cs_link_abc.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Iterable
 
-from polystar.communication.command import Command
+from polystar.communication.command import NO_TARGET_COMMAND, Command, make_target_command
+from polystar.target_pipeline.target_abc import SimpleTarget
 
 
 class CSLinkABC(ABC):
@@ -11,3 +12,9 @@ class CSLinkABC(ABC):
     @abstractmethod
     def send_command(self, command: Command):
         pass
+
+    def send_target(self, target: SimpleTarget):
+        self.send_command(make_target_command(target))
+
+    def send_no_target(self):
+        self.send_command(NO_TARGET_COMMAND)
