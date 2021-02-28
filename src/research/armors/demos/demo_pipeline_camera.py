2424d3d9295fd0c6a2cd884d6e843bb9d756033b
Mathieu Beligon <mathieu@feedly.com>
2021-02-27 20:58:56 -0500

    [demo] add persistency, and send target

--- src/research/armors/demos/demo_pipeline_camera.py
+++ src/research/armors/demos/demo_pipeline_camera.py
@@ -1,5 +1,6 @@
 from injector import inject
 
+from polystar.communication.cs_link_abc import CSLinkABC
 from polystar.dependency_injection import make_injector
 from polystar.frame_generators.frames_generator_abc import FrameGeneratorABC
 from polystar.target_pipeline.debug_pipeline import DebugTargetPipeline
@@ -9,15 +10,21 @@ from polystar.view.cv2_results_viewer import CV2ResultViewer
 
 
 @inject
-def demo_pipeline_on_camera(pipeline: DebugTargetPipeline, webcam: FrameGeneratorABC):
+def demo_pipeline_on_camera(pipeline: DebugTargetPipeline, webcam: FrameGeneratorABC, cs_link: CSLinkABC):
     fps, pipeline_fps = FPS(), FPS()
     with CV2ResultViewer("TensorRT demo") as viewer:
+        persistence_last_detection: int = 0
         for image in webcam.generate():
             pipeline_fps.skip()
             try:
-                pipeline.predict_target(image)
+                target = pipeline.predict_target(image)
+                cs_link.send_target(target)
+                persistence_last_detection = 5
             except NoTargetFoundException:
-                pass
+                if persistence_last_detection:
+                    persistence_last_detection -= 1
+                else:
+                    cs_link.send_no_target()
             pipeline_fps.tick(), fps.tick()
             viewer.add_debug_info(pipeline.debug_info_)
             viewer.add_text(f"FPS: {fps:.1f} / {pipeline_fps:.1f}", 10, 10, (0, 0, 0))
