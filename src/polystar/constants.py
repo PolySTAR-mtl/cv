6e0cbe08294e922a3b556fa5fcb6c2a431b84731
Mathieu Beligon <beligonmathieu@gmail.com>
2021-04-13 17:37:31 -0400

    [constants] Fix circular imports with settings

--- src/polystar/constants.py
+++ src/polystar/constants.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-from polystar.settings import settings
-
-PROJECT_DIR: Path = "/content/drive/My Drive/PolySTAR/3. RoboMaster/Équipe-Computer vision" if settings.PLATFORM == "colab" else Path(
-    __file__
-).parent.parent.parent
+# PROJECT_DIR: Path = (
+#    "/content/drive/My Drive/PolySTAR/3. RoboMaster/Équipe-Computer vision"
+#   if settings.PLATFORM == "colab"
+#   else Path(__file__).parent.parent.parent
+# )
+PROJECT_DIR: Path = Path(__file__).parent.parent.parent
 RESOURCES_DIR: Path = PROJECT_DIR / "resources"
 
 LABEL_MAP_PATH: Path = PROJECT_DIR / "dataset" / "label_map.pbtxt"
