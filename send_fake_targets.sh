5abc983b45387726723ab54fe48499c133b83f62
Mathieu Beligon <beligonmathieu@gmail.com>
2021-05-05 00:18:50 -0400

    WIP

---
+++ send_fake_targets.sh
@@ -0,0 +1,1 @@
+sudo PYTHONPATH=src/ POLYSTAR_ENV=production python src/research/scripts/send_fake_targets.py
--- src/research/scripts/send_fake_targets.py
+++ src/research/scripts/send_fake_targets.py
@@ -1,27 +1,19 @@
-from contextlib import suppress
+from cmath import pi
 from time import sleep
 
 from injector import inject
 
-from polystar.communication.command import make_target_command
 from polystar.communication.cs_link_abc import CSLinkABC
 from polystar.dependency_injection import make_injector
 from polystar.target_pipeline.target_abc import SimpleTarget
 
 
 @inject
-def send_fake_targets_in_loop(cs_link: CSLinkABC, fps: float):
-    target = SimpleTarget(theta=0.25, phi=-0.35, d=5.3)
-
-    with suppress(KeyboardInterrupt):
-        while True:
-            cs_link.send_command(make_target_command(target))
-
-            for c in cs_link.read_commands():
-                print(c)
-
-            sleep(1 / fps)
+def send_fake_targets_in_loop(cs_link: CSLinkABC):
+    for i in range(60):
+        cs_link.send_target(SimpleTarget(d=i / 100, phi=0, theta=pi / 2))
+        sleep(1 / 60)
 
 
 if __name__ == "__main__":
-    make_injector().call_with_injection(send_fake_targets_in_loop, kwargs=dict(fps=0.5))
+    make_injector().call_with_injection(send_fake_targets_in_loop)
